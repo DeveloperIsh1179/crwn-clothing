ce70bc13b2c1f11c6cc71043486202db7866717e
DeveloperIsh1179 <ismael.sanchez.1179@gmail.com>
2022-07-20 22:41:00 -0700

    added reducer to the user context

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,9 +1,7 @@
-import { Outlet } from 'react-router-dom';
 import Directory from 'component/directory/directory.component';
 
 const Home = () => (
   <div>
-    <Outlet />
     <Directory />
   </div>
 );
